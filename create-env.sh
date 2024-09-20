1dfd1797627e7c42a7191721bd5cf406e59b3dff
Tom Peterka <tpeterka@mcs.anl.gov>
2024-09-20 16:33:40 -0500

    Fix env-create.sh
    Install henson before installing the rest, otherwise ftk can't find
    henson

--- create-env.sh
+++ create-env.sh
@@ -25,20 +25,22 @@ spack add diy
 spack add fmt
 
 # the following is for optional ftk consumer w/ CUDA for GPU; comment out if not needed
-# spack add cuda
-# spack add vtk@9.2.2
-# spack add ndarray+hdf5+netcdf+mpi+cuda+vtk
-# # spack develop ftk@mpas+cuda ^ndarray+hdf5+netcdf+mpi+cuda+vtk
-# spack add ftk@mpas+cuda ^ndarray+hdf5+netcdf+mpi+cuda+vtk
+spack add cuda
+spack add vtk@9.2.2
+spack add ndarray+hdf5+netcdf+mpi+cuda+vtk
+# spack develop ftk@mpas+cuda ^ndarray+hdf5+netcdf+mpi+cuda+vtk
+spack add ftk@mpas+cuda ^ndarray+hdf5+netcdf+mpi+cuda+vtk
 
 # the following is for optional ftk consumer w/o CUDA for CPU; comment out if not needed
 # spack add vtk@9.2.2
 # spack add ndarray+hdf5+netcdf+mpi+vtk
-# spack develop ftk@mpas+cuda ^ndarray+hdf5+netcdf+mpi+vtk
+# # spack develop ftk@mpas+cuda ^ndarray+hdf5+netcdf+mpi+vtk
 # spack add ftk@mpas+cuda ^ndarray+hdf5+netcdf+mpi+vtk
 
 # install everything in environment
 echo "installing dependencies in environment"
+spack install henson        # install henson so that henson path is set
+export HENSON=`spack location -i henson`
 spack install
 
 spack env deactivate
